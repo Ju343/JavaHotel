6aefa231209d544c11c79d9384eb84eba0725f6c
Ju343 <julien.gillot25@gmail.com>
2020-01-17 14:39:01 +0100

    Voila

--- src/net/lecnam/nfe103/Client.java
+++ src/net/lecnam/nfe103/Client.java
@@ -1,4 +1,8 @@
 package net.lecnam.nfe103;
 
 public class Client {
+
+
+
+
 }
--- src/net/lecnam/nfe103/Reservation.java
+++ src/net/lecnam/nfe103/Reservation.java
@@ -1,4 +1,49 @@
 package net.lecnam.nfe103;
 
 public class Reservation {
+
+    private int num_reservation;
+    private float date_arrive;
+    private float date_depart;
+    private int nb_voyageurs;
+
+
+    public Reservation(int num_reservation, float date_arrive, float date_depart, int nb_voyageurs) {
+        this.num_reservation = num_reservation;
+        this.date_arrive = date_arrive;
+        this.date_depart = date_depart;
+        this.nb_voyageurs = nb_voyageurs;
+    }
+
+    public int getNum_reservation() {
+        return num_reservation;
+    }
+
+    public void setNum_reservation(int num_reservation) {
+        this.num_reservation = num_reservation;
+    }
+
+    public float getDate_arrive() {
+        return date_arrive;
+    }
+
+    public void setDate_arrive(float date_arrive) {
+        this.date_arrive = date_arrive;
+    }
+
+    public float getDate_depart() {
+        return date_depart;
+    }
+
+    public void setDate_depart(float date_depart) {
+        this.date_depart = date_depart;
+    }
+
+    public int getNb_voyageurs() {
+        return nb_voyageurs;
+    }
+
+    public void setNb_voyageurs(int nb_voyageurs) {
+        this.nb_voyageurs = nb_voyageurs;
+    }
 }
